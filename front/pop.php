fec2e035ca1c3689da26428aa6a1735004ffee9b
Angie <liya0706s@gmail.com>
2024-03-13 16:23:08 +0800

    fix:人氣文章的多少人按讚吃不到good欄位??

--- front/pop.php
+++ front/pop.php
@@ -1,1 +1,82 @@
-pop page
+<fieldset>
+    <legend>目前位置 : 首頁 > 人氣文章區 </legend>
+    <table>
+        <tr>
+            <th style="width:30%">標題</th>
+            <th style="width:50%">內容</th>
+            <th style="width:20%">人氣</th>
+        </tr>
+        <?php
+        $total = $News->count(['sh' => 1]);
+        $div = 5;
+        $pages = ceil($total / $div);
+        $now = $_GET['p'] ?? 1;
+        $start = ($now - 1) * $div;
+
+        $rows = $News->all(['sh' => 1], " limit $start, $div");
+        foreach ($rows as $row) {
+        ?>
+            <tr>
+                <td class="clo t" style="cursor: pointer;" data-id="<?= $row['id']; ?>">
+                    <?= $row['title']; ?>
+                </td>
+                <td>
+                    <div id="s<?= $row['id']; ?>">
+                        <?= mb_substr($row['news'], 0, 25); ?>
+                    </div>
+                    <div id="a<?= $row['id']; ?>" style="display:none;">
+                        <?= $row['news']; ?>
+                    </div>
+                </td>
+
+                <td class="ct">
+                    <?php
+                    $row['good'];
+                    echo "個人說讚";
+                    echo "<img src='./icon/02B03.jpg' style='width:20px'>";
+                    // 登入的按讚紀錄
+                    if (isset($_SESSION['user'])) {
+                        if ($Log->count(['news' => $row['id'], 'acc' => $_SESSION['user']]) > 0) {
+                            echo "<a href='Javascript:good({$row['id']})'> 收回讚 </a>";
+                        } else {
+                            echo "<a href='Javascript:good({$row['id']})'> 讚 </a>";
+                        }
+                    }
+                    ?>
+                </td>
+            </tr>
+        <?php } ?>
+    </table>
+    <?php
+    if (($now - 1) > 0) {
+        $prev = $now - 1;
+        echo "<a href='?do=news&p=$prev'> < </a>";
+    }
+
+    for ($i = 1; $i <= $pages; $i++) {
+        $fontsize = ($i == $now) ? 'font-size:22px' : 'font-size:18px';
+        echo "<a href='?do=news&p=$i' style='$fontsize'> $i </a>";
+    }
+
+    if (($now + 1) >= $pages) {
+        $next = $now + 1;
+        echo "<a href='?do=news&p=$next'> > </a>";
+    }
+    ?>
+</fieldset>
+
+<script>
+    $(".t").on('click', function() {
+        let id = $(this).data('id');
+        $(`#s${id}, #a${id}`).toggle();
+    })
+
+    // 用POST傳值，這裡指的是-按讚紀錄表的news
+    function good(news) {
+        $.post('./api/good.php', {
+            news
+        }, () => {
+            location.reload();
+        })
+    }
+</script>
