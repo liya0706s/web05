c641c4a0a054184521d5be231330a43f1173f020
Angie <liya0706s@gmail.com>
2024-03-13 21:41:01 +0800

    feat:問卷要先做後台

--- front/que.php
+++ front/que.php
@@ -1,1 +1,40 @@
-que page
+<fieldset>
+    <legend>目前位置 : 首頁 > 問卷調查</legend>
+    <table>
+        <tr>
+            <td>編號</td>
+            <td>問卷題目</td>
+            <td>投票總數</td>
+            <td>結果</td>
+            <td>狀態</td>
+        </tr>
+        <?php
+        $rows = $Que->all();
+        foreach ($rows as $row) {
+        ?>
+            <tr>
+                <td><?=$row['id'];?></td>
+                <td><?=$row['title'];?></td>
+                <td><?=$row['vote'];?></td>
+                <td>
+                    <a href="?do=result&id=<?=$row['id'];?>"></a>
+                </td>
+                <td>
+                    <?php
+                    if (!isset($_SESSION['user'])) {
+                    ?>
+                        <span>請先登入</span>
+                    <?php
+                    } else {
+                    ?>
+                        <a href="?do=vote&id=<?=$row['id'];?>">參與投票</a>
+                    <?php
+                    }
+                    ?>
+                </td>
+            </tr>
+        <?php
+        }
+        ?>
+    </table>
+</fieldset>
