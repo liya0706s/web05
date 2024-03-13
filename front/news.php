7aa363c37f190b7a18182534bd55c9d5ac3d1ce8
Angie <liya0706s@gmail.com>
2024-03-13 13:52:40 +0800

    feat: 最新文章打完，要來除錯....

--- front/news.php
+++ front/news.php
@@ -1,1 +1,47 @@
-news page
+<fieldset>
+    <legend>目前位置 : 首頁 > 最新文章區 </legend>
+    <table>
+        <tr>
+            <th>標題</th>
+            <th>內容</th>
+        </tr>
+        <?php
+        $total = $News->count();
+        $div = 5;
+        $pages = ceil($total / $div);
+        $now = $_GET['p'] ?? 1;
+        $start = ($now - 1) * $div;
+
+        $news = $News->all(['sh' => 1], " limit $start, $div");
+        ?>
+        <tr>
+            <td id="t">
+                <?= $news['title']; ?></td>
+            <td>
+                <div class="s<?= $news['id']; ?>">
+                    <?= mb_substr($news['news'], 0, 25); ?>
+                </div>
+                <div class="a<?= $news['id']; ?>">
+                    <?= $news['news']; ?>
+                </div>
+            </td>
+        </tr>
+    </table>
+    <?php
+    if(($now-1)>0){
+        $prev=$now-1;
+        echo "<a href='?do=news&p=$next'> < </a>";
+    }
+
+    for($i=1; $i<=$pages; $i++){
+        $fontsize=($i==$now)?'font-size:22px':'font-size:18px';
+        echo "<a href='?do=news&$i' style='$fontsize'> $i </a>";
+    }
+
+    if(($now+1)>=$pages){
+        $next=$now+1;
+        echo "<a href='?do=news&p=$next'> > </a>";
+    }
+
+    ?>
+</fieldset>
