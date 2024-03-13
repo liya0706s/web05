77ef5a1e22858f3c2d69312a240680f997c3006e
Angie <liya0706s@gmail.com>
2024-03-13 15:01:21 +0800

    fix: log資料表的欄位是id, news, acc

--- front/news.php
+++ front/news.php
@@ -14,40 +14,49 @@
         $start = ($now - 1) * $div;
 
         $rows = $News->all(['sh' => 1], " limit $start, $div");
-        foreach($rows as $row){
+        foreach ($rows as $row) {
         ?>
-        <tr>
-            <td class="clo t" style="cursor:pointer;" data-id="<?=$row['id'];?>">
-                <?= $row['title']; ?>
-            </td>
-            
-            <td>
-                <div id="s<?= $row['id']; ?>">
-                    <?= mb_substr($row['news'], 0, 25) ;?>...
-                </div>
-                <div id="a<?= $row['id']; ?>" style="display: none;">
-                    <?= $row['news']; ?> 
-                </div>
-            </td>
-            <td>
+            <tr>
+                <td class="clo t" style="cursor:pointer;" data-id="<?= $row['id']; ?>">
+                    <?= $row['title']; ?>
+                </td>
 
-            </td>
-        </tr>
+                <td>
+                    <div id="s<?= $row['id']; ?>">
+                        <?= mb_substr($row['news'], 0, 25); ?>...
+                    </div>
+                    <div id="a<?= $row['id']; ?>" style="display: none;">
+                        <?= $row['news']; ?>
+                    </div>
+                </td>
+                <td>
+                    <?php
+                    // 登入的按讚紀錄
+                    if (isset($_SESSION['user'])) {
+                        if ($Log->count(['news' => $row['id'], 'acc' => $_SESSION['user']])>0) {
+                            echo "<a href='Javascript:good({$row['id']})'>收回讚</a>";
+                        } else {
+                            echo "<a href='Javascript:good({$row['id']})'> 讚 </a>";
+                        }
+                    }
+                    ?>
+                </td>
+            </tr>
         <?php } ?>
     </table>
     <?php
-    if(($now-1)>0){
-        $prev=$now-1;
+    if (($now - 1) > 0) {
+        $prev = $now - 1;
         echo "<a href='?do=news&p=$prev'> < </a>";
     }
 
-    for($i=1; $i<=$pages; $i++){
-        $fontsize=($i==$now)?'font-size:22px':'font-size:18px';
+    for ($i = 1; $i <= $pages; $i++) {
+        $fontsize = ($i == $now) ? 'font-size:22px' : 'font-size:18px';
         echo "<a href='?do=news&$i' style='$fontsize'> $i </a>";
     }
 
-    if(($now+1)>=$pages){
-        $next=$now+1;
+    if (($now + 1) >= $pages) {
+        $next = $now + 1;
         echo "<a href='?do=news&p=$next'> > </a>";
     }
 
@@ -55,8 +64,14 @@
 </fieldset>
 
 <script>
-$(".t").on('click',function(){
-    let id=$(this).data('id');
-    $(`#s${id},#a${id}`).toggle();
+    $(".t").on('click', function() {
+        let id = $(this).data('id');
+        $(`#s${id},#a${id}`).toggle();
+    })
+
+    function good(news) {
+    $.post('./api/good.php',{news},()=>{
+    location.reload();
 })
+    }
 </script>
